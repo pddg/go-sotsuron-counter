1b5630d68d0f78144dbc80d5f9240004be01d2d0
Pudding <s.kokuryo@gmail.com>
2018-01-16 16:46:33 +0900

    fix

--- main.go
+++ main.go
@@ -9,5 +9,6 @@ func main() {
 	limit := time.Date(2018, 2, 18, 12, 00, 00, 00, time.Local)
 	now := time.Now()
 	diff := limit.Sub(now)
-	fmt.Printf("卒論提出まで%.0f日と%.0f時間", diff.Hours()/24, diff.Hours()/24/60)
+	hoursInt := int(diff.Hours())
+	fmt.Printf("卒論提出まで%d日と%d時間%d分\n", hoursInt/24, hoursInt%60, int(diff.Seconds())%60)
 }
